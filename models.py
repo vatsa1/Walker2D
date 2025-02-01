169cfca081be21ee6e6b9e197f3db69b5458b821
Shrivatsa Mudligiri <113912739+vatsa1@users.noreply.github.com>
2025-02-01 16:18:10 -0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(Actor, self).__init__()
+        self.layer_1 = nn.Linear(state_dim, 256)
+        self.layer_2 = nn.Linear(256, 256)
+        self.layer_3 = nn.Linear(256, action_dim)
+        self.max_action = max_action
+
+    def forward(self, x):
+        x = torch.relu(self.layer_1(x))
+        x = torch.relu(self.layer_2(x))
+        x = self.max_action * torch.tanh(self.layer_3(x))
+        return x
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+        self.layer_1 = nn.Linear(state_dim + action_dim, 256)
+        self.layer_2 = nn.Linear(256, 256)
+        self.layer_3 = nn.Linear(256, 1)
+
+    def forward(self, x, u):
+        xu = torch.cat([x, u], 1)
+        x = torch.relu(self.layer_1(xu))
+        x = torch.relu(self.layer_2(x))
+        x = self.layer_3(x)
+        return x
